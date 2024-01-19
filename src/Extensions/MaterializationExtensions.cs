19cbcdeeba4979cc253b51efc0da087527c46a67
Olusegun Akinyelure <oakinyelure@bapspace.com>
2024-01-18 21:13:25 -0500

    Updated extension methods to reflect Document and separate it from EFCore

--- src/Extensions/MaterializationExtensions.cs
+++ src/Extensions/MaterializationExtensions.cs
@@ -35,7 +35,7 @@ namespace Cosmos.Entity.Mapper
         /// <param name="cancellationToken"></param>
         /// <param name="source"></param>
         /// <returns></returns>
-        public static async IAsyncEnumerable<TDocument> AsEnumerableAsync<TDocument>(this IQueryable<TDocument> source,[EnumeratorCancellation] CancellationToken cancellationToken = default)
+        public static async IAsyncEnumerable<TDocument> AsDocumentEnumerableAsync<TDocument>(this IQueryable<TDocument> source,[EnumeratorCancellation] CancellationToken cancellationToken = default)
         {
             await foreach(var item in AsMaterializable(source).AsEnumerableAsync(cancellationToken))
             {
@@ -49,7 +49,7 @@ namespace Cosmos.Entity.Mapper
         /// <param name="cancellationToken"></param>
         /// <param name="source"></param>
         /// <returns></returns>
-        public static async Task<List<TDocument>> ToListAsync<TDocument>(this IQueryable<TDocument> source, CancellationToken cancellationToken = default)
+        public static async Task<List<TDocument>> ToDocumentListAsync<TDocument>(this IQueryable<TDocument> source, CancellationToken cancellationToken = default)
             => await AsMaterializable(source).ToListAsync(cancellationToken);
 
 
@@ -62,11 +62,11 @@ namespace Cosmos.Entity.Mapper
         /// <param name="predicate"></param>
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
-        public static async Task<TDocument> FirstOrDefaultAsync<TDocument>(this IQueryable<TDocument> source, Expression<Func<TDocument, bool>> predicate = null, CancellationToken cancellationToken = default)
+        public static async Task<TDocument> FirstOrDefaultDocumentAsync<TDocument>(this IQueryable<TDocument> source, Expression<Func<TDocument, bool>> predicate = null, CancellationToken cancellationToken = default)
             => await source.AsMaterializable().FirstOrDefaultAsync(predicate, cancellationToken);
 
         /// <summary>
-        /// Like <see cref="FirstOrDefaultAsync"/>, this returns the first element in the sequence. However it throws a
+        /// Like <see cref="FirstOrDefaultDocumentAsync"/>, this returns the first element in the sequence. However it throws a
         /// <see cref="InvalidOperationException"/> exception if no item is in the sequence
         /// </summary>
         /// <param name="predicate"></param>
@@ -74,22 +74,22 @@ namespace Cosmos.Entity.Mapper
         /// <param name="source"></param>
         /// <exception cref="InvalidOperationException"></exception>
         /// <returns></returns>
-        public static async Task<TDocument> FirstAsync<TDocument>(this IQueryable<TDocument> source, Expression<Func<TDocument, bool>> predicate = null, CancellationToken cancellationToken = default)
+        public static async Task<TDocument> FirstDocumentAsync<TDocument>(this IQueryable<TDocument> source, Expression<Func<TDocument, bool>> predicate = null, CancellationToken cancellationToken = default)
             => await source.AsMaterializable().FirstAsync(predicate, cancellationToken);
 
         /// <summary>
-        /// Like <see cref="FirstAsync"/>, <see cref="SingleAsync"/> returns the first element in a sequence but throws an <see cref="InvalidOperationException"/> exception
+        /// Like <see cref="FirstDocumentAsync"/>, <see cref="SingleDocumentAsync"/> returns the first element in a sequence but throws an <see cref="InvalidOperationException"/> exception
         /// if there are no element or if there more than one item in the sequence
         /// </summary>
         /// <param name="cancellationToken"></param>
         /// <param name="predicate"></param>
         /// <param name="source"></param>
         /// <returns></returns>
-        public static async Task<TDocument> SingleAsync<TDocument>(this IQueryable<TDocument> source, Expression<Func<TDocument, bool>> predicate = null, CancellationToken cancellationToken = default)
+        public static async Task<TDocument> SingleDocumentAsync<TDocument>(this IQueryable<TDocument> source, Expression<Func<TDocument, bool>> predicate = null, CancellationToken cancellationToken = default)
             => await source.AsMaterializable().SingleAsync(predicate, cancellationToken);
 
         /// <summary>
-        /// Like <see cref="FirstOrDefaultAsync"/>, this also returns the first element in the sequence or null if no item is in the sequence
+        /// Like <see cref="FirstOrDefaultDocumentAsync"/>, this also returns the first element in the sequence or null if no item is in the sequence
         /// <para>However, will throw <see cref="InvalidOperationException"/> if there are more than one item in the sequence</para>
         /// </summary>
         /// <param name="cancellationToken"></param>
@@ -97,7 +97,7 @@ namespace Cosmos.Entity.Mapper
         /// <param name="source"></param>
         /// <exception cref="InvalidOperationException"></exception>
         /// <returns></returns>
-        public static async Task<TDocument> SingleOrDefaultAsync<TDocument>(this IQueryable<TDocument> source, Expression<Func<TDocument, bool>> predicate = null, CancellationToken cancellationToken = default)
+        public static async Task<TDocument> SingleOrDefaultDocumentAsync<TDocument>(this IQueryable<TDocument> source, Expression<Func<TDocument, bool>> predicate = null, CancellationToken cancellationToken = default)
             => await source.AsMaterializable().SingleOrDefaultAsync(predicate, cancellationToken);
 
         /// <summary>
@@ -112,7 +112,7 @@ namespace Cosmos.Entity.Mapper
         /// <param name="cancellationToken"></param>
         /// <param name="source"></param>
         /// <returns></returns>
-        public static async Task<int> CountAsync<TDocument>(this IQueryable<TDocument> source, Expression<Func<TDocument, bool>> predicate = null, CancellationToken cancellationToken = default)
+        public static async Task<int> DocumentCountAsync<TDocument>(this IQueryable<TDocument> source, Expression<Func<TDocument, bool>> predicate = null, CancellationToken cancellationToken = default)
             => await source.AsMaterializable().CountAsync(predicate, cancellationToken);
 
         /// <summary>
@@ -122,7 +122,7 @@ namespace Cosmos.Entity.Mapper
         /// <param name="cancellationToken"></param>
         /// <param name="source"></param>
         /// <returns>Returns true if there is at least one document in the sequence. Returns false otherwise</returns>
-        public static async Task<bool> AnyAsync<TDocument>(this IQueryable<TDocument> source, Expression<Func<TDocument, bool>> predicate = null, CancellationToken cancellationToken = default)
+        public static async Task<bool> AnyDocumentAsync<TDocument>(this IQueryable<TDocument> source, Expression<Func<TDocument, bool>> predicate = null, CancellationToken cancellationToken = default)
             => await source.AsMaterializable().AnyAsync(predicate, cancellationToken);
 
         /// <summary>
@@ -134,7 +134,7 @@ namespace Cosmos.Entity.Mapper
         /// <returns>
         /// <para>A task representing the asynchronous result. Would return <see langword="true"/> if every document in the sequence matches the predicate</para>
         /// </returns>
-        public static async Task<bool> AllAsync<TDocument>(this IQueryable<TDocument> source, Expression<Func<TDocument, bool>> predicate, CancellationToken cancellationToken = default)
+        public static async Task<bool> AllDocumentAsync<TDocument>(this IQueryable<TDocument> source, Expression<Func<TDocument, bool>> predicate, CancellationToken cancellationToken = default)
             => await source.AsMaterializable().AllAsync(predicate, cancellationToken);
     }
 }
